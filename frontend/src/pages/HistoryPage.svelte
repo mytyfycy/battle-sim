214d3a729bbf5e221d7f7b8f107a7f1714771935
mytyfycy <43451809+mytyfycy@users.noreply.github.com>
2026-07-28 14:55:36 +0200

    style(frontend): update styles for HistoryPage.svelte

--- frontend/src/pages/HistoryPage.svelte
+++ frontend/src/pages/HistoryPage.svelte
@@ -19,7 +19,7 @@
         render: (id) => `
           <button
             data-battle-id="${id}"
-            class="px-6 py-3 bg-(--gold) text-black hover:text-(--ink) rounded-lg font-semibold disabled:opacity-50 active:scale-95 transition-all cursor-pointer"
+            class="px-3 py-1 border-2 border-(--panel-border) text-(--ink) hover:text-(--ink-dim) rounded-lg font-semibold disabled:opacity-50 active:scale-95 transition-all cursor-pointer"
             >
               Zobacz
             </button>
@@ -33,13 +33,53 @@
 </script>
 
 <div class="max-w-6xl mx-auto">
-    <div class="px-6 py-3 text-center text-(--ink) bg-(--panel) border rounded-xl">
+    <div class="px-6 py-3 text-center text-(--ink) bg-(--panel) border-2 border-(--panel-border) rounded-xl">
         <DataTable {columns} ajaxUrl="{API_BASE}/battles" onDetailsClick={goToDetails} />
     </div>
 </div>
 
 <style>
-    :global(.dt-length select option) {
-        background-color: var(--panel);
+    :global(.dt-search input),
+    :global(.dt-length select) {
+        border: 2px solid var(--panel-border) !important;
+        background-color: var(--panel) !important;
+        color: inherit;
+        border-radius: 4px !important;
+        padding: 4px 8px !important;
+    }
+
+    :global(.dt-length select option:checked) {
+        background-color: var(--panel-border) !important;
+        color: #fff !important;
+    }
+
+    :global(.dt-paging .dt-paging-button) {
+        border: 2px solid var(--panel-border) !important;
+        border-radius: 4px !important;
+        background: transparent !important;
+    }
+
+    :global(table.dataTable tbody tr:nth-child(even)) {
+        background-color: rgba(255,255,255,0.03) !important;
+    }
+
+    :global(table.dataTable tbody tr:hover) {
+        background-color: rgba(255,255,255, 0.07) !important;
+    }
+
+    :global(table.dataTable thead th) {
+        background-color: var(--panel-border) !important;
+        text-transform: uppercase;
+        font-size: 14px;
+        text-align: center;
+        border-bottom: 2px solid var(--ink-dim) !important;
+    }
+
+    :global(table.dataTable thead th:hover) {
+        background-color: rgba(255,255,255,0.05) !important;
+    }
+
+    :global(table.dataTable thead) {
+        box-shadow: 0 4px 6px -1px rgba(0,0,0,0.3);
     }
 </style>
